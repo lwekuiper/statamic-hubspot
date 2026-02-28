92e8df6e6616debdfc706876d60c7c8cbc979836
Laurens Kuiper <l@urens.me>
2026-02-28 09:20:58 +0100

    fix: namespace stache store handle to avoid conflicts
    Rename the Stache store key from 'form-configs' to
    'hubspot-form-configs' to prevent collisions when multiple
    form-config addons are installed simultaneously.

--- tests/Stache/FormConfigStoreTest.php
+++ tests/Stache/FormConfigStoreTest.php
@@ -20,7 +20,7 @@ class FormConfigStoreTest extends TestCase
     {
         parent::setUp();
 
-        $this->store = Stache::store('form-configs');
+        $this->store = Stache::store('hubspot-form-configs');
     }
 
     #[Test]
